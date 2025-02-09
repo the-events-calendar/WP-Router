e2e58b6c44d7e76394f0b2f9ab14f3e5cf967911
Leane Sturm <76846588+sturmfink@users.noreply.github.com>
2025-02-09 08:45:27 +0100

    Update WP_Router_Sample.class.php
    Removed the depricated use of the function get_class() without input.
    Introduced the new function get_called_class() which returns the name of the class a static method was called in.

--- WP_Router_Sample.class.php
+++ WP_Router_Sample.class.php
@@ -7,7 +7,7 @@
  
 class WP_Router_Sample {
 	public static function init() {
-		add_action('wp_router_generate_routes', array(get_called_class(), 'generate_routes'), 10, 1); //avoid depricated get_class() without input
+		add_action('wp_router_generate_routes', array(get_called_class(), 'generate_routes'), 10, 1);
 	}
 
 	public static function generate_routes( WP_Router $router ) {
